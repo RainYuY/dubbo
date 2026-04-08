f6e6fdb78c1ed95b56e824d0668542b5c58e1bf5
Yukina <rainyu@apache.org>
2026-04-08 10:24:15 +0800

    Remove endpoint identification algorithm setting

--- dubbo-remoting/dubbo-remoting-http3/src/main/java/org/apache/dubbo/remoting/http3/Http3SslContexts.java
+++ dubbo-remoting/dubbo-remoting-http3/src/main/java/org/apache/dubbo/remoting/http3/Http3SslContexts.java
@@ -78,7 +78,6 @@ public final class Http3SslContexts extends SslContext {
             throw new IllegalArgumentException("Could not find certificate file or the certificate is invalid.", t);
         }
         try {
-            builder.endpointIdentificationAlgorithm(null);
             return builder.applicationProtocols(Http3.supportedApplicationProtocols())
                     .build();
         } catch (Throwable t) {
@@ -124,6 +123,7 @@ public final class Http3SslContexts extends SslContext {
         } catch (Throwable t) {
             throw new IllegalArgumentException("Could not find certificate file or the certificate is invalid.", t);
         }
+        builder.endpointIdentificationAlgorithm(null);
         try {
             return builder.applicationProtocols(Http3.supportedApplicationProtocols())
                     .build();
